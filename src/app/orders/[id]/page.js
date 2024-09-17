9ce7c9ed7633071c4e3e266174c226ef03f58cda
TechEm <you@example.com>
2024-09-17 15:17:32 +0200

    update order page

--- src/app/orders/[id]/page.js
+++ src/app/orders/[id]/page.js
@@ -37,13 +37,30 @@ export default function OrderPage() {
   }
 
   return (
-    <section className="max-w-2xl mx-auto mt-8">
-      <div className="text-center">
-        <SectionHeaders mainHeader="Your order" />
-        <div className="mt-4 mb-8">
-          <p>Your orde is on the way...</p>
-          <p>We will call you when your order will be on the way.</p>
+    <section className="hero md:mt-4">
+      <div className="py-8 md:py-12">
+        <h1 className="text-5xl font-semibold">
+        Your orde is on the way 
+          <span className="text-primary">
+          <span> </span>...
+          </span>
+        </h1>
+        <p className="my-6 text-gray-500 text-sm">
+          We will call you when your order will be on the way.
+        </p>
+        <div className="flex gap-4 text-sm">
+          <button className="flex justify-center bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-50">
+            <a 
+              href="/"> 
+              Home
+              </a>
+            <Right />
+          </button>
+        
         </div>
+      </div>
+      <div className="relative hidden md:block">
+        <Image src={'/your_Order.gif'} layout={'fill'} objectFit={'contain'} alt={'your_order'} />
       </div>
 
       <div className="relative hidden md:block">
