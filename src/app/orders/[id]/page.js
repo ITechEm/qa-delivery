8bd692b245301d320bba0d5fd2650d1f4abc2598
TechEm <you@example.com>
2024-09-17 16:55:40 +0200

    update order page

--- src/app/orders/[id]/page.js
+++ src/app/orders/[id]/page.js
@@ -49,9 +49,9 @@ export default function OrderPage() {
         <p className="my-6 text-gray-500 text-sm">
           We will call you when your order will arrive on your address.
         </p>
-        
+        <p>
         <Image src={'/your_Order.gif'} layout={'fill'} objectFit={'contain'} alt={'your_order'} />
-      
+        </p>
         <div className="flex gap-4 text-sm">
           <button className="flex justify-center bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded">
             <a 
