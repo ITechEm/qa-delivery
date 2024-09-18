5bbb706a7c3ff87da5ad5247d5a18b970d50c4d7
TechEm <you@example.com>
2024-09-19 01:08:57 +0200

    change

--- src/app/orders/[id]/asdsa.js
+++ src/app/orders/[id]/asdsa.js
@@ -5,6 +5,8 @@ import SectionHeaders from "@/components/layout/SectionHeaders";
 import CartProduct from "@/components/menu/CartProduct";
 import {useParams} from "next/navigation";
 import {useContext, useEffect, useState} from "react";
+import Right from "@/components/icons/Right";
+import Image from "next/image";
 
 export default function OrderPage() {
   const {clearCart} = useContext(CartContext);
@@ -36,33 +38,49 @@ export default function OrderPage() {
   }
 
   return (
-    <section className="max-w-2xl mx-auto mt-8">
-      <div className="text-center">
-        <SectionHeaders mainHeader="Your order" />
-        <div className="mt-4 mb-8">
-          <p>Thanks for your order.</p>
-          <p>We will call you when your order will be on the way.</p>
+    <section className="hero md:mt-4">
+      <div className="py-8 md:py-12">
+        <h1 className="text-5xl font-semibold">
+        Your order is on the way 
+          <span className="text-primary">
+          <span> </span>...
+          </span>
+        </h1>
+        <p className="my-6 text-gray-500 text-sm">
+          We will call you when your order will arrive on your address.
+        </p>
+        <div className="flex gap-4 text-sm">
+          <button className="flex justify-center bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-50">
+            <a 
+              href="/"> 
+              Home
+              </a>
+            <Right />
+          </button>
         </div>
+      </div>
+      <div className="relative hidden md:block">
+        <Image src={'/your_Order.gif'} layout={'fill'} objectFit={'contain'} alt={'your_order'} />
       </div>
       {loadingOrder && (
         <div>Loading order...</div>
       )}
       {order && (
-        <div className="grid md:grid-cols-2 md:gap-16">
+        <div className="flex grid md:grid-cols-2 md:gap-16 md:block">
           <div>
             {order.cartProducts.map(product => (
               <CartProduct key={product._id} product={product} />
             ))}
             <div className="text-right py-2 text-gray-500">
               Subtotal:
-              <span className="text-black font-bold inline-block w-8">${subtotal}</span>
+              <span className="text-black font-bold inline-block w-8">€{subtotal}</span>
               <br />
               Delivery:
-              <span className="text-black font-bold inline-block w-8">$5</span>
+              <span className="text-black font-bold inline-block w-8">€5</span>
               <br />
               Total:
               <span className="text-black font-bold inline-block w-8">
-                ${subtotal + 5}
+                €{subtotal + 5}
               </span>
             </div>
           </div>
