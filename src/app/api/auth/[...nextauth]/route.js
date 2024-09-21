0be6f8fa36e2e04939b1ba77a5945c3f64626135
TechEm <you@example.com>
2024-09-21 21:48:58 +0200

    login fixes

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -73,20 +73,6 @@ export async function isAdmin() {
   }
 }
 
-export async function getUserInfo(req, res) {
-  const session = await getServerSession(authOptions);
-  if (!session) return res.status(401).json({ message: "Unauthorized" });
-
-  try {
-    const user = await User.findOne({ email: session.user.email }).select('-password'); // Avoid returning password
-    if (!user) return res.status(404).json({ message: "User not found" });
-
-    return res.status(200).json(user);
-  } catch (error) {
-    console.error("Error fetching user information:", error.message);
-    return res.status(500).json({ message: "Internal Server Error" });
-  }
-}
 
 const handler = NextAuth(authOptions);
 
