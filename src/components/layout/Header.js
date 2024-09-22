c81e489a9e66fd38687156b7aeb16157db887806
TechEm <you@example.com>
2024-09-22 22:05:13 +0200

    updates

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -11,7 +11,7 @@ function AuthLinks({ status, userName }) {
     return (
       <>
         <Link href={'/profile'} className="whitespace-nowrap">
-          <h1 className="text-black inknut">Hello, {userName}</h1>
+          <h1 className="text-black inknut text-lenght-10">Hello, {userName}</h1>
         </Link>
         <button
           onClick={() => signOut()}
