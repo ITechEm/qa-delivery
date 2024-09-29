a9619387989cc635a63d029417bb7ef83ec7ca07
TechEm <you@example.com>
2024-09-29 22:23:43 +0200

    updates

--- src/app/orders/[id]/page.js
+++ src/app/orders/[id]/page.js
@@ -57,11 +57,12 @@ export default function OrderPage() {
               </a>
             <Right />
           </button>
+          <Image src={'/your_Order.gif'} layout={'fill'} objectFit={'contain'} alt={'your_order'} />
         </div>
       </div>
-      <div className="relative md:block">
+      {/* <div className="relative md:block">
         <Image src={'/your_Order.gif'} layout={'fill'} objectFit={'contain'} alt={'your_order'} />
-      </div>
+      </div> */}
       {loadingOrder && (
         <div className="text-center inria">Loading order...</div>
       )}
