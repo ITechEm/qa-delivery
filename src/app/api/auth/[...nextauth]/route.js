189d57fd50825cdb35e238ce65ef93a0a767ee43
TechEm <you@example.com>
2024-09-30 00:27:37 +0200

    updates

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -7,39 +7,39 @@ import NextAuth, { getServerSession } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 
-// export const authOptions = {
-//   secret: process.env.SECRET,
-//   adapter: MongoDBAdapter(clientPromise),
-//   session: {
-//     strategy: "jwt",
-//     maxAge: 30 * 24 * 60 * 60 // 30 days
-//   },
-//   providers: [
-//     CredentialsProvider({
-//       name: 'Credentials',
-//       id: 'credentials',
-//       credentials: {
-//         username: { label: "Email", type: "email", placeholder: "test@example.com" },
-//         password: { label: "Password", type: "password" },
-//       },
+export const authOptions = {
+  secret: process.env.SECRET,
+  adapter: MongoDBAdapter(clientPromise),
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60 // 30 days
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      id: 'credentials',
+      credentials: {
+        username: { label: "Email", type: "email", placeholder: "test@example.com" },
+        password: { label: "Password", type: "password" },
+      },
       
-//       async authorize(credentials, req) {
-//         const email = credentials?.email;
-//         const password = credentials?.password;
+      async authorize(credentials, req) {
+        const email = credentials?.email;
+        const password = credentials?.password;
 
-//         mongoose.connect(process.env.MONGO_URL);
-//         const user = await User.findOne({email});
-//         const passwordOk = user && bcrypt.compareSync(password, user.password);
+        mongoose.connect(process.env.MONGO_URL);
+        const user = await User.findOne({email});
+        const passwordOk = user && bcrypt.compareSync(password, user.password);
 
-//         if (passwordOk) {
-//           return user;
-//         }
+        if (passwordOk) {
+          return user;
+        }
 
-//         return null
-//       }
-//     })
-//   ],
-// };
+        return null
+      }
+    })
+  ],
+};
 
 // export async function isAdmin() {
 //   const session = await getServerSession(authOptions);
@@ -59,65 +59,6 @@ import { MongoDBAdapter } from "@auth/mongodb-adapter";
 // export { handler as GET, handler as POST }
 
 
-
-
-
-
-export const authOptions = {
-  secret: process.env.SECRET,
-  adapter: MongoDBAdapter(clientPromise),
-  session: {
-    strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60 // 30 days
-  },
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      id: 'credentials',
-      credentials: {
-        username: { label: "Email", type: "email", placeholder: "test@example.com" },
-        password: { label: "Password", type: "password" },
-      },
-      
-      async authorize(credentials, req) {
-        // Check for null pointer references
-        if (!credentials || !credentials.email || !credentials.password) {
-          return null;
-        }
-
-        const { email, password } = credentials;
-
-        // Connect to database
-        try {
-          await mongoose.connect(process.env.MONGO_URL);
-        } catch (error) {
-          console.error('Error while connecting to database:', error);
-          return null;
-        }
-
-        // Find the user
-        try {
-          const user = await User.findOne({ email });
-          if (!user) {
-            return null;
-          }
-
-          // Compare password
-          const passwordValid = await bcrypt.compare(password, user.password);
-          if (!passwordValid) {
-            return null;
-          }
-
-          // Return user if authentication is successful
-          return user;
-        } catch (error) {
-          console.error('Error while authenticating:', error);
-          return null;
-        }
-      }
-    })
-  ],
-};
 
 
 // export async function POST(req) {
