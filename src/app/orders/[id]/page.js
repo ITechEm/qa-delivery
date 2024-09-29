0caa6aa05bbce96add1874925e4a14be08e5561c
TechEm <you@example.com>
2024-09-29 22:06:43 +0200

    updates

--- src/app/orders/[id]/page.js
+++ src/app/orders/[id]/page.js
@@ -50,7 +50,7 @@ export default function OrderPage() {
           We will call you when your order will arrive on your address.
         </p>
         <div className="felx">
-        <Image src={'/your_Order.gif'} layout={'fill'} objectFit={'contain'} alt={'your_order'} />
+        <Image src={'/your_Order.gif'} layout={'fill'} objectFit={'contain'} alt={'your_order'} width={200} height={200} />
       </div>
         <div className="flex gap-4 text-sm">
           <button className="flex inknut justify-center bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-50">
