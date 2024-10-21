d9392fafaf07a1c8043f178adb8a0522cc34c819
TechEm <you@example.com>
2024-10-21 23:05:23 +0200

    login update

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -34,7 +34,7 @@ export const authOptions = {
         if (passwordOk) {
           return user;
         }
-        if (!credentials || !credentials.email || !credentials.password) {
+        if (!credentials || !credentials?.email || !credentials?.password) {
           return Response.json(
         { message: "Email or password invalid" },
         { status: 400 }
