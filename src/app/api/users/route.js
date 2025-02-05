f70a85506b225f6bbe075f29598d93551101940c
TechEm <you@example.com>
2025-02-05 03:34:13 +0100

    user api update

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -2,54 +2,64 @@ import {isAdmin} from "@/app/api/auth/[...nextauth]/route";
 import {User} from "@/models/User";
 import mongoose from "mongoose";
 
-export async function GET(req) {
-  const { searchParams } = new URL(req.url);
-  const userId = searchParams.get('_id');
+// export async function GET(req) {
+//   const { searchParams } = new URL(req.url);
+//   const userId = searchParams.get('_id');
 
-  if (!userId) {
-    return new Response(
-      JSON.stringify({ message: "User ID is required" }),
-      {
-        status: 400,
-        headers: { 'Content-Type': 'application/json' },
-      }
-    );
-  }
+//   if (!userId) {
+//     return new Response(
+//       JSON.stringify({ message: "User ID is required" }),
+//       {
+//         status: 400,
+//         headers: { 'Content-Type': 'application/json' },
+//       }
+//     );
+//   }
 
-  try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+//   try {
+//     await mongoose.connect(process.env.MONGO_URL, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
 
-    const user = await User.findById(userId);
-    if (!user) {
-      return new Response(
-        JSON.stringify({ message: "User not found" }),
-        {
-          status: 404,
-          headers: { 'Content-Type': 'application/json' },
-        }
-      );
-    }
+//     const user = await User.findById(userId);
+//     if (!user) {
+//       return new Response(
+//         JSON.stringify({ message: "User not found" }),
+//         {
+//           status: 404,
+//           headers: { 'Content-Type': 'application/json' },
+//         }
+//       );
+//     }
 
-    return new Response(
-      JSON.stringify(user),
-      {
-        status: 200,
-        headers: { 'Content-Type': 'application/json' },
-      }
-    );
-  } catch (error) {
-    console.error(error);
-    return new Response(
-      JSON.stringify({ message: "Internal Server Error" }),
-      {    status: 500,
-        headers: { 'Content-Type': 'application/json' },
-      }
-    );
-  } finally {
-    mongoose.connection.close();
+//     return new Response(
+//       JSON.stringify(user),
+//       {
+//         status: 200,
+//         headers: { 'Content-Type': 'application/json' },
+//       }
+//     );
+//   } catch (error) {
+//     console.error(error);
+//     return new Response(
+//       JSON.stringify({ message: "Internal Server Error" }),
+//       {    status: 500,
+//         headers: { 'Content-Type': 'application/json' },
+//       }
+//     );
+//   } finally {
+//     mongoose.connection.close();
+//   }
+// }
+
+export async function GET() {
+  mongoose.connect(process.env.MONGO_URL);
+  if (await isAdmin()) {
+    const users = await User.find();
+    return Response.json(users);
+  } else {
+    return Response.json([]);
   }
 }
 
